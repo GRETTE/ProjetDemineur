8a55ce222e1ef51b71e189846ab589561c26d639
GRETTE <yanael.grette@outlook.com>
2017-03-18 20:28:36 +0100

    Création du projet Demineur.

---
+++ app/src/main/java/demineur/projetdemineur/MainActivity.java
@@ -0,0 +1,13 @@
+package demineur.projetdemineur;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
