b6b11fb8d6f3fbda981b53a742b745fde8210e80
GRETTE <yanael.grette@outlook.com>
2017-03-19 18:56:05 +0100

    Implémentation du MainActivity.

--- app/src/main/java/demineur/projetdemineur/Vue/MainActivity.java
+++ app/src/main/java/demineur/projetdemineur/Vue/MainActivity.java
@@ -1,8 +1,12 @@
 package demineur.projetdemineur.Vue;
 
 import android.app.Activity;
+import android.app.Application;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 import demineur.projetdemineur.R;
 
@@ -12,5 +16,29 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnJouer = (Button)findViewById(R.id.buttonPlay);
+        btnJouer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ChoixDifficulte.class);
+                startActivity(intent);
+            }
+        });
+        Button btnInstruction = (Button)findViewById(R.id.buttonInstruction);
+        btnInstruction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Instruction.class);
+                startActivity(intent);
+            }
+        });
+        Button btnQuit = (Button)findViewById(R.id.buttonQuit);
+        btnQuit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
