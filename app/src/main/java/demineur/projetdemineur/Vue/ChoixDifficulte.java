34ec622d1ccf839ea120a8ee5dbc772d9da7b808
GRETTE <yanael.grette@outlook.com>
2017-03-19 19:14:02 +0100

    Implémentation du ChoixDifficulte.

--- app/src/main/java/demineur/projetdemineur/Vue/ChoixDifficulte.java
+++ app/src/main/java/demineur/projetdemineur/Vue/ChoixDifficulte.java
@@ -1,8 +1,11 @@
 package demineur.projetdemineur.Vue;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 import demineur.projetdemineur.R;
 
@@ -11,10 +14,48 @@ import demineur.projetdemineur.R;
  */
 
 public class ChoixDifficulte extends Activity {
-
+    private static int DIFFICULTE;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.difficulte);
+
+        Button btnFacile= (Button) findViewById(R.id.buttonFacile);
+        btnFacile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDifficulte(1);
+                Intent intent = new Intent(ChoixDifficulte.this,Demineur.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnNormal= (Button) findViewById(R.id.buttonNormal);
+        btnNormal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDifficulte(2);
+                Intent intent = new Intent(ChoixDifficulte.this,Demineur.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnDifficile= (Button) findViewById(R.id.buttonDifficile);
+        btnDifficile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setDifficulte(3);
+                Intent intent = new Intent(ChoixDifficulte.this,Demineur.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public static int getDifficule() {
+        return DIFFICULTE;
+    }
+
+    private static void setDifficulte(int i) {
+        DIFFICULTE = i;
     }
 }
